51f66ec6c7dabf05aa6ec78ba2970ef6d568d3be
Salockin <salockin9@gmail.com>
2026-02-03 20:00:46 -0500

    shader source files refactor

--- CChessGUI/dragVertex.glsl
+++ CChessGUI/dragVertex.glsl
@@ -3,13 +3,14 @@
 layout (location = 0) in vec2 aPosition;
 layout (location = 1) in vec2 aTexCoord;
 
-uniform float dragX;
-uniform float dragY;
+uniform vec2 dragPosition;
 
-out vec2 TexCoord;
+out vec2 texCoord;
+
+
 
 void main()
 {
-	gl_Position = vec4(aPosition + vec2(dragX, dragY), 0.0, 1.0);
-	TexCoord = aTexCoord;
+	gl_Position = vec4(aPosition + dragPosition, 0.0, 1.0);
+	texCoord = aTexCoord;
 }

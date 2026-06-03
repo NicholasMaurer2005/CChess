65c9827ffeccc0ea5bd49eed328b1a91990635e1
Salockin <salockin9@gmail.com>
2026-06-02 22:08:11 -0400

    fixed bugs

--- CChess/MoveList.hpp
+++ CChess/MoveList.hpp
@@ -44,7 +44,7 @@ private:
 		return score + (static_cast<int>(move.promotePiece()) << 7);
 	}
 
-	static bool MoveGreater(Move lhs, Move rhs, Move killerMove1 Move killerMove2, Move pvMove) noexcept
+	static bool MoveGreater(Move lhs, Move rhs, Move killerMove1, Move killerMove2, Move pvMove) noexcept
 	{
 		const bool lhsKiller{ lhs.move() == killerMove1.move() || lhs.move() == killerMove2.move() };
 		const bool lhsIsPvMove{ lhs.move() == pvMove.move() };
@@ -131,48 +131,48 @@ public:
 	//setters
 	void sort(Move killerMove1, Move killerMove2, Move pvMove) noexcept
 	{
-		std::sort(m_moves.begin(), m_back, [killerMoves, pvMove](Move lhs, Move rhs) { return MoveGreater(lhs, rhs, killerMove1, killerMove2, pvMove); });
+		std::sort(m_moves.begin(), m_back, [killerMove1, killerMove2, pvMove](Move lhs, Move rhs) { return MoveGreater(lhs, rhs, killerMove1, killerMove2, pvMove); });
 	}
 
 	template<Piece piece>
 	void pushQuiet(int sourceIndex, int destinationIndex) noexcept 
 	{
-		*m_back = makeQuiet<piece>(sourceIndex, destinationIndex);
+		*m_back = Move::makeQuiet<piece>(sourceIndex, destinationIndex);
 		++m_back;
 	}
 
 	template<Piece piece>
 	void pushAttack(Piece attackPiece, int sourceIndex, int destinationIndex) noexcept 
 	{
-		*m_back = makeAttack<piece>(attackPiece, sourceIndex, destinationIndex);
+		*m_back = Move::makeAttack<piece>(attackPiece, sourceIndex, destinationIndex);
 		++m_back;
 	}
 
 	template<Piece sourcePiece, Piece promotePiece>
 	void pushQuietPromote(int sourceIndex, int destinationIndex) noexcept 
 	{
-		*m_back = makeQuietPromote<sourcePiece, promotePiece>(sourceIndex, destinationIndex);
+		*m_back = Move::makeQuietPromote<sourcePiece, promotePiece>(sourceIndex, destinationIndex);
 		++m_back;
 	}
 
 	template<Piece sourcePiece, Piece promotePiece>
 	void pushAttackPromote(Piece attackPiece, int sourceIndex, int destinationIndex) noexcept 
 	{
-		*m_back = makeAttackPromote<sourcePiece, promotePiece>(attackPiece, sourceIndex, destinationIndex);
+		*m_back = Move::makeAttackPromote<sourcePiece, promotePiece>(attackPiece, sourceIndex, destinationIndex);
 		++m_back;
 	}
 
 	template<Piece sourcePiece, Piece attackPiece>
 	void pushEnpassant(int sourceIndex, int destinationIndex, int enpassantIndex) noexcept 
 	{
-		*m_back = makeEnpassant<sourcePiece, attackPiece>(sourceIndex, destinationIndex, enpassantIndex);
+		*m_back = Move::makeEnpassant<sourcePiece, attackPiece>(sourceIndex, destinationIndex, enpassantIndex);
 		++m_back;
 	}
 
 	template<Piece piece>
 	void pushDoublePawn(int sourceIndex, int destinationIndex) noexcept 
 	{
-		*m_back = makeDoublePawn<piece>(sourceIndex, destinationIndex);
+		*m_back = Move::makeDoublePawn<piece>(sourceIndex, destinationIndex);
 		++m_back;
 	}
 
